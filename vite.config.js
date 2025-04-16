c9b2fcf6cc41140d67e5cb232b0b2c18ce2303b9
Saurabh667 <saurav000667@gmail.com>
2025-04-16 14:39:48 +0530

    fixing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/QUOTES-BUZZ/',
+  base: '/https://github.com/Saurabh667/QUOTES-BUZZ.git/',
   plugins: [react()],
 })
